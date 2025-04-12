17046a7bc20f8be00f04c6c8c2287f1eed73c2f2
Santiago López <santaigolopezocm@gmail.com>
2025-04-11 20:21:56 -0500

    fix: rename secp256k1 target to avoid conflict with web3

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         .target(
             name: "SolanaSwift",
             dependencies: [
-                .product(name: "secp256k1", package: "secp256k1.swift"),
+                .product(name: "secp256k1_solana", package: "secp256k1.swift"),
                 .product(name: "TweetNacl", package: "tweetnacl-swiftwrap"),
                 .product(name: "Task_retrying", package: "task-retrying-swift"),
             ]
